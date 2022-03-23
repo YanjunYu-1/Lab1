9eab2d012eaea1ccffa725bfb4c9d099d2cfab96
Yanjun Yu <yanjunyu@student.mitt.ca>
2022-03-23 18:04:30 -0500

    添加项目文件。

---
+++ Lab1/Models/Client.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab1.Models
+{
+    public class Client
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(25, MinimumLength = 3)]
+        public string FirstName { get; set; }
+
+        [StringLength(25, MinimumLength = 3)]
+        public string LastName { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"/^\\(?([0-9]{3})\\)?[-.\\s]?([0-9]{3})[-.\\s]?([0-9]{4})$/")]
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ Lab1/Models/Room.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static System.Collections.Specialized.BitVector32;    
+
+namespace Lab1.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+
+        [Key]
+        public int RoomNumber { get; set; }
+        public int Capacity { get; set; }
+        public Section section { get; set; }
+
+        public enum Section//部分
+        {
+            First,
+
+            Second,
+
+            Third
+        }
+    }
+}
